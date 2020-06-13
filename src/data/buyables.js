56a613fd2717d86438641dfa8c2417dda8299140
Daniel Whiffing <danielwhiffing@gmail.com>
2020-06-13 15:20:39 -0400

    Updates data

--- src/data/buyables.js
+++ src/data/buyables.js
@@ -1,8 +1,13 @@
 export const buyables = [
   {
+    id: 'buySeat',
+    label: '+ seat',
+    cost: { wood: 10 },
+  },
+  {
     id: 'createBuilding',
     label: '+ building',
-    cost: { wood: 10 },
+    cost: { wood: 50 },
   },
   {
     id: 'buyCity',
@@ -10,43 +15,38 @@ export const buyables = [
     cost: { wood: 100 },
   },
   {
-    id: 'buySeat',
-    label: '+ seat',
-    cost: { wood: 10 },
-  },
-  {
-    id: 'exploreGalaxy',
-    label: 'explore systems',
-    cost: { wood: 10 },
-  },
-  {
-    id: 'exploreSystem',
-    label: 'explore stars',
-    cost: { wood: 10 },
-  },
-  {
-    id: 'explorePlanet',
-    label: 'explore ocean',
-    cost: { wood: 10 },
-  },
-  {
     id: 'exploreContinent',
     label: 'explore',
-    cost: { wood: 10 },
-  },
-  {
-    id: 'settleSystem',
-    label: 'settle system',
-    cost: { wood: 10 },
-  },
-  {
-    id: 'settlePlanet',
-    label: 'settle planet',
-    cost: { wood: 10 },
+    cost: { wood: 1000 },
   },
   {
     id: 'settleContinent',
     label: 'settle continent',
-    cost: { wood: 10 },
+    cost: { wood: 500 },
+  },
+  {
+    id: 'explorePlanet',
+    label: 'explore ocean',
+    cost: { wood: 10000 },
+  },
+  {
+    id: 'settlePlanet',
+    label: 'settle planet',
+    cost: { wood: 5000 },
+  },
+  {
+    id: 'exploreSystem',
+    label: 'explore stars',
+    cost: { wood: 100000 },
+  },
+  {
+    id: 'settleSystem',
+    label: 'settle system',
+    cost: { wood: 50000 },
+  },
+  {
+    id: 'exploreGalaxy',
+    label: 'explore systems',
+    cost: { wood: 1000000 },
   },
 ]
